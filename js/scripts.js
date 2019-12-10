3f0f34b6907f34fb9e2dabbd268e75c579bffe8f
LiliiaDudchenko <dudchenkolilia@gmail.com>
2019-12-10 12:36:21 +0200

    homework16
    Added slider-images, second block, resronsive.

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+
+$('.navigation').on('click', '.mobile_menu', function () {
+    $(".navigation ul").slideToggle(400);
+});
+
+
+
+
+
+
+
+ jQuery(document).ready(function () {
+  var mainSlider = new Swiper ('.swiper-container', {
+    autoplay: true,
+    loop: true,
+    speed: 1000
+
+  })
+});
