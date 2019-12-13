19d442abfe137744ef696ea9b378c553a1050d4f
LiliiaDudchenko <dudchenkolilia@gmail.com>
2019-12-13 12:28:01 +0200

    Homework17

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,27 @@
-
 $('.navigation').on('click', '.mobile_menu', function () {
-    $(".navigation ul").slideToggle(400);
+  $(".navigation ul").slideToggle(400);
 });
 
 
 
-
-
-
-
- jQuery(document).ready(function () {
-  var mainSlider = new Swiper ('.swiper-container', {
+jQuery(document).ready(function () {
+  var mainSlider = new Swiper('.swiper-container', {
     autoplay: true,
     loop: true,
     speed: 1000
 
+  })
+});
+
+jQuery(document).ready(function () {
+  var textSlider = new Swiper('.swiper-container', {
+
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    autoplay: true,
+    loop: true,
+    speed: 1000
   })
 });
